85b1fa463d339b443569c7c9a3cd6cb1e9404079
viven12 <vivenrakesh@gmail.com>
2024-06-25 23:45:10 +0530

    Commit-1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-WORK_DIR=/home/ubuntu/ilp
+WORK_DIR=/home/jenkins/ilp
 mkdir -p $WORK_DIR
-chown -vc ubuntu:www-data $WORK_DIR || exit 1
+# chown -vc ubuntu:www-data $WORK_DIR || exit 1
 cd $WORK_DIR
-[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv
-chown -R ubuntu:www-data venv || exit 1
+[[ -d venv ]] || /usr/bin/virtualenv -p /usr/bin/python3 venv 
+# chown -R ubuntu:www-data venv || exit 1
 # chown -R ubuntu:www-data ~/.npm || exit 1
-chown -R ubuntu:www-data /usr/lib/node_modules || exit 1
+# chown -R ubuntu:www-data /usr/lib/node_modules || exit 1
