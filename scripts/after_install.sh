0db5e210dcc3d74f4576113394abdeb935c31355
viven12 <vivenrakesh@gmail.com>
2024-07-04 13:47:04 +0530

    Defect in Boundry Report Solved

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,8 +5,8 @@ source venv/bin/activate || exit 1
 pip3 install -r requirements/base.txt --exists-action w || exit 1
 pip3 install gunicorn || exit 1
 cp -fv /home/jenkins/prod_settings.py ${WORK_DIR}/ilp/settings/
-python3 manage.py migrate || exit 1
-python3 manage.py loaddata apps/*/fixtures/*.json || exit 1
+# python3 manage.py migrate || exit 1
+# python3 manage.py loaddata apps/*/fixtures/*.json || exit 1
 python3 manage.py collectstatic -c --noinput || exit 1  
 #sudo npm install -g newman || exit 1
 ##sudo npm install -g newman-reporter-html || exit 1     
