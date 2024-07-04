67a0916d4d103d8f9c3cffdd29379d307bdb425b
viven12 <vivenrakesh@gmail.com>
2024-07-04 13:46:18 +0530

    Defect in Boundry Report Solved

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo systemctl stop ka.service || exit 1
-sudo systemctl stop od.service || exit 1
-sudo systemctl stop jk.service || exit 1
+sudo systemctl stop ka_gunicorn.service || exit 1
+sudo systemctl stop od_gunicorn.service || exit 1
+# sudo systemctl stop jk.service || exit 1
